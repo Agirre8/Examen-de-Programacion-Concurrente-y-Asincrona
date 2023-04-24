95f11ab2befd686d8a50b098191d1626b804591c
Agirre8 <inagi30@gmail.com>
2023-04-24 18:12:48 +0200

    +

--- Archivos/Ejercicio2.py
+++ Archivos/Ejercicio2.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
-
+#Bueno, para utilizar executors y actualizar el saldo de los jugadores, debemos modificar la clase Jugador 
+# y la clase Banco para utilizar el patrón Observer y asegurarnos de que el saldo de cada jugador se actualice 
+# correctamente después de cada ronda.
 
 class Observer(ABC):
     @abstractmethod
@@ -75,14 +77,17 @@ class Jugador(Observer):
         with ThreadPoolExecutor(max_workers=4) as executor:
             resultados = executor.map(self.jugar_numero, [random.randint(1, 36) for _ in range(4)], [self.saldo] * 4, [banco] * 4)
         self.saldo = sum(resultados)
+        print(resultados)
 
         # Jugando par/impar
         with ThreadPoolExecutor(max_workers=4) as executor:
             resultados = executor.map(self.jugar_par_impar, ["par"] * 2 + ["impar"] * 2, [self.saldo] * 4, [banco] * 4)
         self.saldo = sum(resultados)
+        print(resultados)
 
         # Jugando con martingala
         with ThreadPoolExecutor(max_workers=4) as executor:
             resultados = executor.map(self.jugar_martingala, [random.randint(1, 36) for _ in range(4)], [self.saldo] * 4, [banco] * 4)
         self.saldo = sum(resultados)
+        print(resultados)
     
